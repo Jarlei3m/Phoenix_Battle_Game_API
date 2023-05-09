de4ca3d40218320beb1f54134db2836bba0ec37c
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-05-09 07:45:03 -0300

    feat: refactoring trainers controller

--- lib/ex_mon_web/controllers/trainers_json.ex
+++ lib/ex_mon_web/controllers/trainers_json.ex
@@ -17,4 +17,18 @@ defmodule ExMonWeb.TrainersJSON do
       }
     }
   end
+
+  def render("show.json", %{
+        trainer: %Trainer{
+          id: id,
+          name: name,
+          inserted_at: inserted_at
+        }
+      }) do
+    %{
+      id: id,
+      name: name,
+      inserted_at: inserted_at
+    }
+  end
 end
