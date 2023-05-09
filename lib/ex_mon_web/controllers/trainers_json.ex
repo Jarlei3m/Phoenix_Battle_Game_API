d9fb63dfeba3bca790ab1cfa8fb6af0404558eb9
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-05-08 21:15:47 -0300

    feat: rendering trainers

---
+++ lib/ex_mon_web/controllers/trainers_json.ex
@@ -0,0 +1,20 @@
+defmodule ExMonWeb.TrainersJSON do
+  alias ExMon.Trainer
+
+  def render("create.json", %{
+        trainer: %Trainer{
+          id: id,
+          name: name,
+          inserted_at: inserted_at
+        }
+      }) do
+    %{
+      message: "Trainer created!",
+      trainer: %{
+        id: id,
+        name: name,
+        inserted_at: inserted_at
+      }
+    }
+  end
+end
