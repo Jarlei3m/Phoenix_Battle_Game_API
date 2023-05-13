1497f2906561ed359a5f41fc7260f3d2b84bd516
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-05-13 08:08:29 -0300

    feat: trainer pokemons controller

---
+++ lib/ex_mon_web/controllers/trainer_pokemons_json.ex
@@ -0,0 +1,28 @@
+defmodule ExMonWeb.TrainerPokemonsJSON do
+  alias ExMon.Trainer.Pokemon
+
+  def render("create.json", %{
+        pokemon: %Pokemon{
+          id: id,
+          name: name,
+          nickname: nickname,
+          weight: weight,
+          types: types,
+          trainer_id: trainer_id,
+          inserted_at: inserted_at
+        }
+      }) do
+    %{
+      message: "Pokemon created!",
+      pokemon: %{
+        id: id,
+        name: name,
+        nickname: nickname,
+        weight: weight,
+        types: types,
+        trainer_id: trainer_id,
+        inserted_at: inserted_at
+      }
+    }
+  end
+end
